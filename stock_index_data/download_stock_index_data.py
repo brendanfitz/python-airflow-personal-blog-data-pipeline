9d97739ef87084ef9005c713149a6a3d9418977c
brendanfitz <brendanjfitzpatrick0@gmail.com>
2020-10-23 21:16:48 +0000

    Remove cleartbl cmd line arg and delete all rows by index automatically

--- stock_index_data/download_stock_index_data.py
+++ stock_index_data/download_stock_index_data.py
@@ -18,8 +18,10 @@ def main(args):
             create_query = f.read()
         cur.execute(create_query)
 
-        if args.cleartbl:
-            cur.execute("DELETE FROM visual.index_component_stocks")
+        # delete old data
+        delete_stmt = ("DELETE FROM visuals.index_component_stocks "
+                       "WHERE stock_index_name = %s")
+        cur.execute(delete_stmt, (args.index, ))
 
         for row in scraper.data_to_tuples():
             insert_stmt = ("INSERT INTO visuals.index_component_stocks "
@@ -36,10 +38,6 @@ def parse_args():
     description = "download stock index data from s3"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("index", help="dowjones or sp500")
-    parser.add_argument("--cleartbl",
-        action="store_true",
-        help="clear table before loading"
-    )
     args = parser.parse_args()
     return args
 
