2d18292be937f7aca67198dbd772966a26a7fe2f
brendanfitz <brendanjfitzpatrick0@gmail.com>
2020-10-26 20:10:10 +0000

    Deleted old stock index data files

--- stock_index_data/create_index_component_stocks_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS visuals.index_component_stocks (
-    stock_index_name varchar(255),
-    symbol varchar(255),
-    company varchar(255),
-    weight float8,
-    price float8,
-    industry varchar(255),
-    industry_weight float8 
-)
