4826b2f6968916ced9e04f3b3e2342b49f7f017f
brendanfitz <brendanjfitzpatrick0@gmail.com>
2020-10-29 16:42:14 +0000

    Make dag dynamic for both indices and add default params

--- airflow/dags/index_component_stocks_dag.py
+++ airflow/dags/index_component_stocks_dag.py
@@ -10,33 +10,29 @@ import sys
 sys.path.insert(0, "/home/brendan/Github/python-airflow-personal-blog-data-pipeline/index_component_stocks")
 from stock_index_scraper import StockIndexScraper
 
-default_args = dict(
-    owner='airflow',
-    depends_on_past=False,
-    catchup=False,
-    start_date='2020-10-25',
-    retries=0,
-    # retry_delay=timedelta(minutes=5)
-)
+def fetch_stock_industries(**kwargs):
+    params = kwargs['params']
+    stock_index_name = params['stock_index_name']
+    from_s3 = params['from_s3']
 
-dag = DAG(
-    'index_component_stocks',
-    default_args=default_args,
-    description='loads and cleans data for index stock component blog visuals',
-    schedule_interval=None,
-)
-
-def fetch_stock_industries(stock_index_name):
-    scraper = StockIndexScraper(stock_index_name, from_s3=True, load_all=False)
+    scraper = StockIndexScraper(stock_index_name, from_s3=from_s3, load_all=False)
     filepath = scraper.scrape_stock_industries(save_to_file=True)
     return filepath
 
-def fetch_stock_prices(stock_index_name):
-    scraper = StockIndexScraper(stock_index_name, from_s3=True, load_all=False)
+def fetch_stock_prices(**kwargs):
+    params = kwargs['params']
+    stock_index_name = params['stock_index_name']
+    from_s3 = params['from_s3']
+
+    scraper = StockIndexScraper(stock_index_name, from_s3=from_s3, load_all=False)
     filepath = scraper.scrape_index_component_stocks(save_to_file=True)
     return filepath
 
-def clean_and_merge_industries(stock_index_name, **kwargs):
+def clean_and_merge_industries(**kwargs):
+    params = kwargs['params']
+    stock_index_name = params['stock_index_name']
+    from_s3 = params['from_s3']
+
     ti = kwargs['ti']
 
     prices_filename = ti.xcom_pull(
@@ -51,13 +47,17 @@ def clean_and_merge_industries(stock_index_name, **kwargs):
     )
     df_industries = pd.read_csv(industries_filename, index_col='Symbol')
 
-    scraper = StockIndexScraper(stock_index_name, from_s3=True, load_all=False)
+    scraper = StockIndexScraper(stock_index_name, from_s3=from_s3, load_all=False)
     df = scraper.clean_df_scraped_and_merge_industries(df_prices, df_industries)
     filepath = scraper.save_df_to_file(df, f"{stock_index_name}__clean")
 
     return filepath
 
-def load_data(stock_index_name, **kwargs):
+def load_data(**kwargs):
+    params = kwargs['params']
+    stock_index_name = params['stock_index_name']
+    from_s3 = params['from_s3']
+
     ti = kwargs['ti']
 
     filename = ti.xcom_pull(
@@ -65,7 +65,7 @@ def load_data(stock_index_name, **kwargs):
         task_ids="clean_and_merge_industries"
     )
 
-    scraper = StockIndexScraper(stock_index_name, from_s3=True, load_all=False)
+    scraper = StockIndexScraper(stock_index_name, from_s3=from_s3, load_all=False)
     scraper.df = pd.read_csv(filename, index_col='Symbol')
     scraper.data = scraper.create_data()
 
@@ -89,7 +89,6 @@ def load_data(stock_index_name, **kwargs):
 
     return {'row_count': row_count}
 
-
 def cleanup(**kwargs):
     ti = kwargs['ti']
 
@@ -111,42 +110,70 @@ def cleanup(**kwargs):
     for filename in [prices_filename, industries_filename, cleaned_filename]:
         remove(filename)
 
+def create_stock_index_components_dag(stock_index_name):
+    default_args = dict(
+        owner='airflow',
+        depends_on_past=False,
+        catchup=False,
+        start_date='2020-10-25',
+        retries=0,
+        # retry_delay=timedelta(minutes=5)
+        params={
+            'stock_index_name': stock_index_name,
+            'from_s3': False,
+        }
+    )
 
-fetch_stock_industries_task = PythonOperator(
-    task_id='fetch_stock_industries',
-    python_callable=fetch_stock_industries,
-    op_kwargs={'stock_index_name': 'dowjones'},
-    dag=dag,
-)
+    dag = DAG(
+        f"{stock_index_name}___index_component_stocks",
+        default_args=default_args,
+        description='loads and cleans data for index stock component blog visuals',
+        schedule_interval=None,
+    )
 
-fetch_stock_prices_task = PythonOperator(
-    task_id='fetch_stock_prices',
-    python_callable=fetch_stock_prices,
-    op_kwargs={'stock_index_name': 'dowjones'},
-    dag=dag,
-)
+    fetch_stock_industries_task = PythonOperator(
+        task_id='fetch_stock_industries',
+        python_callable=fetch_stock_industries,
+        provide_context=True,
+        dag=dag,
+    )
 
-clean_and_merge_industries_task = PythonOperator(
-    task_id="clean_and_merge_industries",
-    python_callable=clean_and_merge_industries,
-    provide_context=True,
-    op_kwargs={'stock_index_name': 'dowjones'},
-    dag=dag,
-)
+    fetch_stock_prices_task = PythonOperator(
+        task_id='fetch_stock_prices',
+        python_callable=fetch_stock_prices,
+        provide_context=True,
+        dag=dag,
+    )
 
-load_data_task = PythonOperator(
-    task_id="load_data",
-    python_callable=load_data,
-    provide_context=True,
-    op_kwargs={'stock_index_name': 'dowjones'},
-    dag=dag,
-)
+    clean_and_merge_industries_task = PythonOperator(
+        task_id="clean_and_merge_industries",
+        python_callable=clean_and_merge_industries,
+        provide_context=True,
+        dag=dag,
+    )
 
-cleanup_task = PythonOperator(
-    task_id="cleanup_task",
-    python_callable=cleanup,
-    provide_context=True,
-    dag=dag,
-)
+    load_data_task = PythonOperator(
+        task_id="load_data",
+        python_callable=load_data,
+        provide_context=True,
+        dag=dag,
+    )
 
-[fetch_stock_industries_task, fetch_stock_prices_task] >> clean_and_merge_industries_task >> load_data_task >> cleanup_task
+    cleanup_task = PythonOperator(
+        task_id="cleanup_task",
+        python_callable=cleanup,
+        provide_context=True,
+        dag=dag,
+    )
+
+    (
+        [fetch_stock_industries_task, fetch_stock_prices_task] >>
+        clean_and_merge_industries_task >>
+        load_data_task >>
+        cleanup_task
+    )
+
+    return dag
+
+dow_jones_dag = create_stock_index_components_dag('dowjones')
+sp500_dag = create_stock_index_components_dag('sp500')
